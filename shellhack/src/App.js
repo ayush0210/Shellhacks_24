03ffc5d0dbd343349e797e8bfc1c1288df28079e
malay1410 <malay1410@gmail.com>
2024-09-28 16:59:46 -0400

    Final Push

--- shellhack/src/App.js
+++ shellhack/src/App.js
@@ -1,7 +1,12 @@
 import React from 'react';
+
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+
 import UploadPage from './components/UploadPage';
 import SummaryPage from './components/SummaryPage';
+import ReadingPage from './components/ReadingPage';
+import QuizPage from './components/QuizPage';
+
 import './App.css';
 
 function App() {
@@ -12,6 +17,8 @@ function App() {
           <Routes>
             <Route path="/" element={<UploadPage />} />
             <Route path="/summary" element={<SummaryPage />} />
+            <Route path="/reading" element={<ReadingPage />} />
+            <Route path="/quiz" element={<QuizPage />} />
           </Routes>
         </header>
       </div>
